19e663e1c920912a52c36b5557251fa995fd3a87
abner323 <abnergonlima@gmail.com>
2026-03-12 09:12:13 -0300

    Update Mainactivity.kt

--- atividade1/Mainactivity.kt
+++ atividade1/Mainactivity.kt
@@ -1,67 +1,108 @@
-package com.example.atividade1b
+package edu.example.atividade 1 b
+
+import androidx.compose.runtime.mutableStateOf
+
+
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.atividade1b.ui.theme.Atividade1BTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            Atividade1BTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+            Column(modifier = Modifier.padding(16.dp)) {
+
+                MessageCard(
+                    Message("Android", "Jetpack Compose", R.drawable.carro)
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                MessageCard(
+                    Message("Abner", "estudando Jetpack Compose", R.drawable.moto)
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                MessageCard(
+                    Message("Lexi", "Compose é legal!", R.drawable.celular)
+                )
             }
         }
     }
 }
 
+data class Message(
+    val author: String,
+    val body: String,
+    val image: Int
+)
+
 @Composable
 fun MessageCard(msg: Message) {
     Row(modifier = Modifier.padding(all = 8.dp)) {
+
         Image(
-            painter = painterResource(R.drawable.profile_picture),
-            contentDescription = "Contact profile picture",
+            painter = painterResource(msg.image),
+            contentDescription = "Foto de perfil",
             modifier = Modifier
-
                 .size(40.dp)
-
                 .clip(CircleShape)
         )
 
         Spacer(modifier = Modifier.width(8.dp))
 
         Column {
+
             Text(text = msg.author)
+
             Spacer(modifier = Modifier.height(4.dp))
+
             Text(text = msg.body)
         }
     }
 }
 
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun PreviewMessageCard() {
-    atividade 1 b{
-        Surface {
-            MessageCard(
-                msg = Message("Lexi", "Dê uma olhada no Jetpack Compose, é ótimo!"")
-            )
-        }
+
+    Column(modifier = Modifier.padding(16.dp)) {
+
+        MessageCard(
+            Message("Lexi", "Hey, take a look at Jetpack Compose!", R.drawable.carro)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        MessageCard(
+            Message("Abner", "Jetpack Compose é muito util!", R.drawable.moto)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        MessageCard(
+            Message("Android", "Bem-vindo ao Compose", R.drawable.celular)
+        )
     }
 }
